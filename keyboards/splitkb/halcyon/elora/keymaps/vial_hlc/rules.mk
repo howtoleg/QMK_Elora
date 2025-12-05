eae3947cbd700e86bcdbf5ad12b3ea683fe32a8f
howtoleg <83886489+howtoleg@users.noreply.github.com>
2025-12-05 12:45:38 -0500

    Update rules.mk

--- keyboards/splitkb/halcyon/elora/keymaps/vial_hlc/rules.mk
+++ keyboards/splitkb/halcyon/elora/keymaps/vial_hlc/rules.mk
@@ -2,6 +2,8 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 
+POINTING_DEVICE_ENABLE = yes
+
 ENCODER_MAP_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
